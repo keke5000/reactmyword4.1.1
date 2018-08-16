e15a79ef29cbf083b3e898fd69211f1f4da3057a
LoserPanda <loserpanda82@gmail.com>
2018-08-16 14:51:06 +0300

    siirtyma jeah

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, {Component} from 'react';
 import './App.css';
-import Header from "../../reactmyworld4.1.1/src/components/Header";
+import Header from "./components/Header";
 import BrowserRouter from "react-router-dom/es/BrowserRouter";
 import Switch from "react-router-dom/es/Switch";
-import Home from "../../reactmyworld4.1.1/src/components/Home";
+import Home from "./components/Home";
 import Route from "react-router-dom/es/Route";
-import Navigation from "./Navigation";
+// import Navigation from "./Navigation";
 import Grid from "react-bootstrap/es/Grid";
 import NotFound from './NotFound';
-import Sidebar from 'react-sidebar';
-import CityList from '../../reactmyworld4.1.1/src/components/CityList';
+// import Sidebar from 'react-sidebar';
+import CityList from './components/CityList';
 import CountryList from "./components/CountryList";
 import ReadCountry from "./components/ReadCountry";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sidebarOpen: true
-        };
-        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-    }
-
-    onSetSidebarOpen(open) {
-        this.setState({sidebarOpen: open});
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         sidebarOpen: true
+    //     };
+    //     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+    // }
+    //
+    // onSetSidebarOpen(open) {
+    //     this.setState({sidebarOpen: open});
+    // }
 
     render() {
         return (
@@ -34,15 +34,15 @@ class App extends Component {
                         <Header/>
                     </div>
                     <hr/>
-                    <Sidebar
-                        sidebar={<Navigation/>}
-                        open={this.state.sidebarOpen}
-                        onSetOpen={this.onSetSidebarOpen}
-                        styles={{sidebar: {background: "white"}}}>
-                        <button onClick={() => this.onSetSidebarOpen(true)}>
-                            Open sidebar
-                        </button>
-                    </Sidebar>
+                    {/*<Sidebar*/}
+                        {/*sidebar={<Navigation/>}*/}
+                        {/*open={this.state.sidebarOpen}*/}
+                        {/*onSetOpen={this.onSetSidebarOpen}*/}
+                        {/*styles={{sidebar: {background: "white"}}}>*/}
+                        {/*<button onClick={() => this.onSetSidebarOpen(true)}>*/}
+                            {/*Open sidebar*/}
+                        {/*</button>*/}
+                    {/*</Sidebar>*/}
                     <Grid>
                         <Switch>
                             <Route exact path="/" component={Home}/>
