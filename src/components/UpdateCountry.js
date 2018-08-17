19e9a33e2e2c2d5c447ae687e2b3cf84f2cc8482
Reija Jokinen <reija.jokinen@kolumbus.fi>
2018-08-17 10:45:17 +0300

    update päivitystä

--- src/components/UpdateCountry.js
+++ src/components/UpdateCountry.js
@@ -1,41 +1,68 @@
 import React, {Component} from 'react'
 
 class UpdateCountry extends Component {
+    state = {country: []};
 
     componentDidMount() {
-        fetch('/api/countries/' + this.props.match.params.code)
-            .then(result => {
-                // console.log("resu", result.json());
-                return result.json();
-            })
+        this.getCountryData();
     }
 
+    getCountryData = () => {
+        let itse = this;
+        fetch('/api/countries/' + this.props.match.params.code)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((olio) => {
+                itse.setState({country: olio});
+            });
+    };
+
     handleNameChange = (event) => {
-        this.setState({ author: event.target.value })
+        this.setState({ name: event.target.value })
+    }
+
+    handleCodeChange = (event) => {
+        this.setState({ code: event.target.value })
+    }
+
+    handlePopulationChange = (event) => {
+        this.setState({ population: event.target.value })
+    }
+
+    handleHeadofstateChange = (event) => {
+        this.setState({ headOfState: event.target.value })
     }
 
     handleCreateClick = (e) => {
         e.preventDefault();
-            this.setState({ quotetext: '', author: '', formErrors: { text: '', author: '' } });
+            this.setState({ code: '', name: '', population: '', headOfState: '' });
     }
 
     render(){
         return(
             <div className="countryform">
-                <form className="country">
-                    <input type="text" placeholder="Country" id="form_name" minLength="1" className="form-control"
-                           value={this.state.name} onChange={this.handleNameChange} />
-                    <input type="text" placeholder="Code" id="form_code" minLength="1" className="form-control"
-                           value={this.state.code} onChange={this.handleCodeChange} />
-                    <input type="text" placeholder="Population" id="form_popul" minLength="1" className="form-control"
-                           value={this.state.population} onChange={this.handlePopulChange} />
-                    <input type="text" placeholder="HeadOfState" id="form_headofstate" minLength="1" className="form-control"
-                           value={this.state.headofstate} onChange={this.handleHeadofstateChange} />
-                    <div>
-                        <input type="submit" value="Update" onClick={this.handleCreateClick} className="form-control btn-primary" />
+                <h2>{this.state.country.name}</h2>
+                <div className="parent">
+                    <div className="narrow">
+                        <h3>Update country</h3>
+                        <form className="country">
+                            <input type="text" id="form_name" minLength="1"
+                                   value={this.state.country.name} onChange={this.handleNameChange}/>
+                            <input type="text" id="form_code" minLength="1" value={this.state.country.code}
+                                   onChange={this.handleCodeChange}/>
+                            <input type="text" id="form_population" minLength="1"
+                                   value={this.state.country.population} onChange={this.handlePopulationChange}/>
+                            <input type="text" id="form_headofstate" minLength="1"
+                                   value={this.state.country.headOfState} onChange={this.handleHeadofstateChange}/>
+                            <div>
+                                <input type="submit" value="Update" onClick={this.handleCreateClick}
+                                       className="form-control btn-primary"/>
+                            </div>
+                        </form>
+                        <hr/>
                     </div>
-                </form>
-            <hr/>
+                </div>
             </div>
         )
     }
